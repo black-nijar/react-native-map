6c82334f613bf1d0bc6db0d508108bdd46bc8871
Nijarudeen <nijarr2020@gmail.com>
2021-01-02 18:34:38 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome To RN_MAP</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
